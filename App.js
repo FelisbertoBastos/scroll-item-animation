48851f4607343353f70264b6f9f852f680a246c7
Felisberto Bastos <felisbertobastos22@gmail.com>
2023-02-21 19:16:44 -0300

    Add animation to flatlist

--- App.js
+++ App.js
@@ -1,20 +1,135 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// Inspiration: https://dribbble.com/shots/14154226-Rolodex-Scrolling-Animation/attachments/5780833?mode=media
+// Photo by Sharefaith from Pexels
+// Background image: https://www.pexels.com/photo/pink-rose-closeup-photography-1231265/
 
-export default function App() {
+// import * as React from "react";
+import { useRef } from "react";
+import {
+  StatusBar,
+  FlatList,
+  Image,
+  Animated,
+  Text,
+  View,
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+  Easing,
+  SafeAreaViewBase,
+  SafeAreaView,
+  Pressable,
+} from "react-native";
+const { width, height } = Dimensions.get("screen");
+// import faker from "faker";
+import { faker } from "@faker-js/faker";
+
+faker.seed(10);
+const DATA = [...Array(30).keys()].map((_, i) => {
+  return {
+    key: faker.datatype.uuid(),
+    image: `https://randomuser.me/api/portraits/${faker.helpers.arrayElement([
+      "women",
+      "men",
+    ])}/${faker.datatype.number(60)}.jpg`,
+    name: faker.name.firstName(),
+    jobTitle: faker.name.jobTitle(),
+    email: faker.internet.email(),
+  };
+});
+
+const BG_IMG =
+  "https://images.pexels.com/photos/1231265/pexels-photo-1231265.jpeg";
+
+const SPACING = 20;
+const AVATAR_SIZE = 70;
+const ITEM_SIZE = AVATAR_SIZE + SPACING * 3;
+
+export default () => {
+  const scrollY = useRef(new Animated.Value(0)).current;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "#fff" }}>
+      <StatusBar />
+      <Image
+        source={{ uri: BG_IMG }}
+        style={StyleSheet.absoluteFillObject}
+        blurRadius={80}
+      />
+      <Animated.FlatList
+        data={DATA}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+          { useNativeDriver: true }
+        )}
+        keyExtractor={(item) => item.key}
+        contentContainerStyle={{
+          padding: SPACING,
+          paddingBottom: 0,
+          // paddingTop: StatusBar.currentHeight || 42,
+        }}
+        renderItem={({ item, index }) => {
+          const inputRange = [
+            -1,
+            0,
+            ITEM_SIZE * index,
+            ITEM_SIZE * (index + 2),
+          ];
+
+          const opacityInputRange = [
+            -1,
+            0,
+            ITEM_SIZE * index,
+            ITEM_SIZE * (index + 1),
+          ];
+
+          const scale = scrollY.interpolate({
+            inputRange,
+            outputRange: [1, 1, 1, 0],
+          });
+
+          const opacity = scrollY.interpolate({
+            inputRange: opacityInputRange,
+            outputRange: [1, 1, 1, 0],
+          });
+
+          return (
+            <Animated.View
+              style={{
+                flexDirection: "row",
+                padding: SPACING,
+                marginBottom: SPACING,
+                // backgroundColor: "rgba(255, 255, 255, 0.8)",
+                backgroundColor: "white",
+                borderRadius: 16,
+                elevation: 10,
+                opacity,
+                transform: [{ scale }],
+              }}
+            >
+              <Image
+                source={{ uri: item.image }}
+                style={{
+                  width: AVATAR_SIZE,
+                  height: AVATAR_SIZE,
+                  borderRadius: AVATAR_SIZE,
+                  marginRight: SPACING / 2,
+                }}
+              />
+              <View>
+                <Text style={{ fontSize: 22, fontWeight: "700" }}>
+                  {item.name}
+                </Text>
+                {/* <Text style={{ fontSize: 18, opacity: 0.7 }}>
+                  {item.jobTitle}
+                </Text> */}
+                <Text style={{ fontSize: 14, opacity: 0.8, color: "#0099CC" }}>
+                  {item.email}
+                </Text>
+              </View>
+            </Animated.View>
+          );
+        }}
+      />
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
